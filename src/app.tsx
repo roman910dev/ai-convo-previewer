1f11f0923640890bac7ed777ba454fd734ce7678
Román Via-Dufresne Saus <roman910dev@gmail.com>
2025-12-17 12:25:10 +0100

    feat: :sparkles: add json input field

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,64 @@
-import ChatBotDemo from "./pages";
+import type { UIMessage } from 'ai'
+import { useState } from 'react'
+
+import ChatbotDemo from './components/chatbot-demo'
+import { Textarea } from './components/ui/textarea'
+import { cn } from './lib/utils'
+
+type State =
+	| {
+			success: true
+			messages: UIMessage[]
+			error?: undefined
+	  }
+	| {
+			success: false
+			error: string
+			messages?: undefined
+	  }
 
 function App() {
-  return <ChatBotDemo />;
+	const [state, setState] = useState<State>({
+		success: true,
+		messages: [],
+	})
+	const onMessagesChange = (input: string) => {
+		try {
+			const messages = JSON.parse(input)
+
+			console.log('updating...')
+			setState({ success: true, messages })
+		} catch (error) {
+			console.error(error)
+			setState({ success: false, error: 'Invalid JSON' })
+		}
+	}
+
+	return (
+		<div className="flex">
+			<div className="flex h-dvh w-full max-w-2xl flex-col border-r">
+				<Textarea
+					className="w-full flex-1 rounded-none border-none font-mono"
+					onChange={(e) => onMessagesChange(e.target.value)}
+				/>
+				<div className="w-full border-t px-4 py-2">
+					{
+						<div
+							className={cn(
+								'font-mono',
+								state.error ? 'text-destructive' : (
+									'text-transparent'
+								),
+							)}
+						>
+							{state.error ?? '.'}
+						</div>
+					}
+				</div>
+			</div>
+			<ChatbotDemo messages={state.messages ?? []} />
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/chatbot-demo.tsx
@@ -0,0 +1,249 @@
+'use client'
+
+import type { ChatStatus, UIMessage } from 'ai'
+import { CopyIcon, GlobeIcon, RefreshCcwIcon } from 'lucide-react'
+import { useState } from 'react'
+
+import {
+	Conversation,
+	ConversationContent,
+	ConversationScrollButton,
+} from '#/components/ai-elements/conversation'
+import { Loader } from '#/components/ai-elements/loader'
+import {
+	Message,
+	MessageAction,
+	MessageActions,
+	MessageContent,
+	MessageResponse,
+} from '#/components/ai-elements/message'
+import {
+	PromptInput,
+	PromptInputActionAddAttachments,
+	PromptInputActionMenu,
+	PromptInputActionMenuContent,
+	PromptInputActionMenuTrigger,
+	PromptInputAttachment,
+	PromptInputAttachments,
+	PromptInputBody,
+	PromptInputButton,
+	PromptInputFooter,
+	PromptInputHeader,
+	PromptInputSelect,
+	PromptInputSelectContent,
+	PromptInputSelectItem,
+	PromptInputSelectTrigger,
+	PromptInputSelectValue,
+	PromptInputSubmit,
+	PromptInputTextarea,
+	PromptInputTools,
+} from '#/components/ai-elements/prompt-input'
+import {
+	Reasoning,
+	ReasoningContent,
+	ReasoningTrigger,
+} from '#/components/ai-elements/reasoning'
+import {
+	Source,
+	Sources,
+	SourcesContent,
+	SourcesTrigger,
+} from '#/components/ai-elements/sources'
+
+const models = [
+	{
+		name: 'GPT 4o',
+		value: 'openai/gpt-4o',
+	},
+	{
+		name: 'Deepseek R1',
+		value: 'deepseek/deepseek-r1',
+	},
+]
+const ChatbotDemo = ({ messages }: { messages: UIMessage[] }) => {
+	const [input, setInput] = useState('')
+	const [model, setModel] = useState<string>(models[0].value)
+	const [webSearch, setWebSearch] = useState(false)
+	// const { messages, sendMessage, status, regenerate } = useChat();
+	const status = 'ready' as ChatStatus
+	const regenerate = () => {}
+	const handleSubmit = () => {}
+	return (
+		<div className="relative size-full h-dvh">
+			<div className="flex h-full flex-col">
+				<Conversation className="h-full">
+					<ConversationContent>
+						{messages.map((message) => (
+							<div key={message.id}>
+								{message.role === 'assistant' &&
+									message.parts.filter(
+										(part) => part.type === 'source-url',
+									).length > 0 && (
+										<Sources>
+											<SourcesTrigger
+												count={
+													message.parts.filter(
+														(part) =>
+															part.type ===
+															'source-url',
+													).length
+												}
+											/>
+											{message.parts
+												.filter(
+													(part) =>
+														part.type ===
+														'source-url',
+												)
+												.map((part, i) => (
+													<SourcesContent
+														key={`${message.id}-${i}`}
+													>
+														<Source
+															key={`${message.id}-${i}`}
+															href={part.url}
+															title={part.url}
+														/>
+													</SourcesContent>
+												))}
+										</Sources>
+									)}
+								{message.parts.map((part, i) => {
+									switch (part.type) {
+										case 'text':
+											return (
+												<Message
+													key={`${message.id}-${i}`}
+													from={message.role}
+												>
+													<MessageContent>
+														<MessageResponse>
+															{part.text}
+														</MessageResponse>
+													</MessageContent>
+													{message.role ===
+														'assistant' &&
+														i ===
+															messages.length -
+																1 && (
+															<MessageActions>
+																<MessageAction
+																	onClick={() =>
+																		regenerate()
+																	}
+																	label="Retry"
+																>
+																	<RefreshCcwIcon className="size-3" />
+																</MessageAction>
+																<MessageAction
+																	onClick={() =>
+																		navigator.clipboard.writeText(
+																			part.text,
+																		)
+																	}
+																	label="Copy"
+																>
+																	<CopyIcon className="size-3" />
+																</MessageAction>
+															</MessageActions>
+														)}
+												</Message>
+											)
+										case 'reasoning':
+											return (
+												<Reasoning
+													key={`${message.id}-${i}`}
+													className="w-full"
+													isStreaming={
+														status ===
+															'streaming' &&
+														i ===
+															message.parts
+																.length -
+																1 &&
+														message.id ===
+															messages.at(-1)?.id
+													}
+												>
+													<ReasoningTrigger />
+													<ReasoningContent>
+														{part.text}
+													</ReasoningContent>
+												</Reasoning>
+											)
+										default:
+											return null
+									}
+								})}
+							</div>
+						))}
+						{status === 'submitted' && <Loader />}
+					</ConversationContent>
+					<ConversationScrollButton />
+				</Conversation>
+				<PromptInput
+					onSubmit={handleSubmit}
+					className="p-4 pt-0"
+					globalDrop
+					multiple
+				>
+					<PromptInputHeader>
+						<PromptInputAttachments>
+							{(attachment) => (
+								<PromptInputAttachment data={attachment} />
+							)}
+						</PromptInputAttachments>
+					</PromptInputHeader>
+					<PromptInputBody>
+						<PromptInputTextarea
+							onChange={(e) => setInput(e.target.value)}
+							value={input}
+						/>
+					</PromptInputBody>
+					<PromptInputFooter>
+						<PromptInputTools>
+							<PromptInputActionMenu>
+								<PromptInputActionMenuTrigger />
+								<PromptInputActionMenuContent>
+									<PromptInputActionAddAttachments />
+								</PromptInputActionMenuContent>
+							</PromptInputActionMenu>
+							<PromptInputButton
+								variant={webSearch ? 'default' : 'ghost'}
+								onClick={() => setWebSearch(!webSearch)}
+							>
+								<GlobeIcon size={16} />
+								<span>Search</span>
+							</PromptInputButton>
+							<PromptInputSelect
+								onValueChange={(value) => {
+									setModel(value)
+								}}
+								value={model}
+							>
+								<PromptInputSelectTrigger>
+									<PromptInputSelectValue />
+								</PromptInputSelectTrigger>
+								<PromptInputSelectContent>
+									{models.map((model) => (
+										<PromptInputSelectItem
+											key={model.value}
+											value={model.value}
+										>
+											{model.name}
+										</PromptInputSelectItem>
+									))}
+								</PromptInputSelectContent>
+							</PromptInputSelect>
+						</PromptInputTools>
+						<PromptInputSubmit
+							disabled={!input && !status}
+							status={status}
+						/>
+					</PromptInputFooter>
+				</PromptInput>
+			</div>
+		</div>
+	)
+}
+export default ChatbotDemo
