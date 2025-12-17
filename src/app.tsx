ed40566b9bbff899c66fa5f0d9b072980ee0246c
Román Via-Dufresne Saus <roman910dev@gmail.com>
2025-12-17 12:54:55 +0100

    feat: :sparkles: add unique message ids

--- src/app.tsx
+++ src/app.tsx
@@ -10,28 +10,51 @@ type State =
 			success: true
 			messages: UIMessage[]
 			error?: undefined
+			errorDetails?: undefined
 	  }
 	| {
 			success: false
 			error: string
+			errorDetails: string
 			messages?: undefined
 	  }
+
+function fallback<T, Fallback>(
+	fn: () => T,
+	fallback: (error: unknown) => Fallback,
+) {
+	try {
+		return fn()
+	} catch (e) {
+		return fallback(e)
+	}
+}
 
 function App() {
 	const [state, setState] = useState<State>({
 		success: true,
 		messages: [],
 	})
-	const onMessagesChange = (input: string) => {
-		try {
-			const messages = JSON.parse(input)
 
-			console.log('updating...')
-			setState({ success: true, messages })
-		} catch (error) {
-			console.error(error)
-			setState({ success: false, error: 'Invalid JSON' })
-		}
+	const makeError = (error: string, details?: string) =>
+		setState({ success: false, error, errorDetails: details ?? '' })
+
+	const onMessagesChange = (input: string) => {
+		const data = fallback(
+			(): UIMessage[] => JSON.parse(input),
+			(e) => makeError('Invalid JSON', e?.toString()),
+		)
+		if (!data) return
+		const uniqueIdMessages = fallback(
+			() =>
+				data.map((message) => ({
+					...message,
+					id: window.crypto.randomUUID(),
+				})),
+			(e) => makeError('Invalid messages', e?.toString()),
+		)
+		if (!uniqueIdMessages) return
+		setState({ success: true, messages: uniqueIdMessages })
 	}
 
 	return (
@@ -41,19 +64,15 @@ function App() {
 					className="w-full flex-1 rounded-none border-none font-mono"
 					onChange={(e) => onMessagesChange(e.target.value)}
 				/>
-				<div className="w-full border-t px-4 py-2">
-					{
-						<div
-							className={cn(
-								'font-mono',
-								state.error ? 'text-destructive' : (
-									'text-transparent'
-								),
-							)}
-						>
-							{state.error ?? '.'}
-						</div>
-					}
+				<div
+					title={state.errorDetails}
+					className={cn(
+						'w-full border-t px-4 py-2 font-mono',
+						state.errorDetails && 'cursor-help',
+						state.error ? 'text-destructive' : 'text-transparent',
+					)}
+				>
+					{state.error ?? '.'}
 				</div>
 			</div>
 			<ChatbotDemo messages={state.messages ?? []} />
